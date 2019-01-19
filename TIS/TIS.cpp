56a91405731703f99cea0a703124701be74c7979
Nguyen <nguyenn15@student.douglascollege.ca>
2019-01-19 14:26:55 -0800

    template tis server, still not workig properly for static files

--- TIS/TIS.cpp
+++ TIS/TIS.cpp
@@ -1,7 +1,57 @@
-#include <Core/Core.h>
+#include <Skylark/Skylark.h>
 
 using namespace Upp;
 
+SKYLARK(HomePage, "")
+{
+	http << "<!DOCTYPE html><html><body>Parameter: " << http[0] << "</html></body>";
+}
+
+SKYLARK(Admin, "admin")
+{
+	http.RenderResult("admin/index.html");
+}
+
+
+SKYLARK(Param, "*/param")
+{
+	http << "<!DOCTYPE html><html><body>Parameter: " << http[0] << "</html></body>";
+}
+
+SKYLARK(Params, "params/**")
+{
+	http << "<!DOCTYPE html><html><body>Parameters: ";
+	for(int i = 0; i < http.GetParamCount(); i++)
+		http << http[i] << " ";
+	http << "</html></body>";
+}
+
+SKYLARK(CatchAll, "**")
+{
+	http.Redirect(HomePage);
+}
+
+SKYLARK(Favicon, "/favicon.ico")
+{
+	http.ContentType("image/png") << LoadFile(GetDataFile("favicon.png"));
+}
+
+struct MyApp : SkylarkApp {
+	MyApp() {
+		root = "tis";
+	#ifdef _DEBUG
+		prefork = 0;
+		use_caching = false;
+	#endif
+	}
+};
+
 CONSOLE_APP_MAIN
 {
+#ifdef _DEBUG
+	StdLogSetup(LOG_FILE|LOG_COUT);
+	Ini::skylark_log = true;
+#endif
+
+	MyApp().Run();	
 }
