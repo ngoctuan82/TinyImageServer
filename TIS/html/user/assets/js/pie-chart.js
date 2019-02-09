de5722711f117e0ad16b285b11a626fcdaf705ed
ngoc tuan nguyen <reactdemo@reactdemo@>
2019-02-09 12:02:05 -0800

    user ui pages

--- TIS/html/user/assets/js/pie-chart.js
+++ TIS/html/user/assets/js/pie-chart.js
@@ -428,35 +428,4 @@ if ($('#seolinechart8').length) {
         }
     });
 }
-
-
-if ($('#totaluserschart').length) {
-    var ctx = document.getElementById("totaluserschart").getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'doughnut',
-        // The data for our dataset
-        data: {
-            labels: ["Active", "Deleted"],
-            datasets: [{
-                backgroundColor: [
-                    
-                    "#12C498",
-                    "#F8CB3F"
-                ],
-                borderColor: '#fff',
-                data: [ 12, 5],
-            }]
-        },
-        // Configuration options go here
-        options: {
-            legend: {
-                display: true
-            },
-            animation: {
-                easing: "easeInOutBack"
-            }
-        }
-    });
-}
 /*-------------- 7 Pie chart chartjs end ------------*/
